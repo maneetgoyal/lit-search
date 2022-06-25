9c02c104a25c5796f9a6372940d8b7cb2a5554cb
Maneet Goyal <maneetgoyal14@gmail.com>
2022-06-25 23:12:26 +0530

    Prettify code

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 export function App(): JSX.Element {
   return (
     <AppBar position="static">
-  <Toolbar variant="dense">
-    <Typography variant="h6" color="inherit" component="div">
-      LitSearch
-    </Typography>
-  </Toolbar>
-</AppBar>
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit" component="div">
+          LitSearch
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
