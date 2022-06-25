1aed43915cf4420645fe25f0bc3222dd2d4cc6a1
Maneet Goyal <maneetgoyal14@gmail.com>
2022-06-25 23:09:07 +0530

    Add AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Grid, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export function App(): JSX.Element {
   return (
-    <Grid>
-      <Typography>
-      App Page
-      </Typography>
-    </Grid>
+    <AppBar position="static">
+  <Toolbar variant="dense">
+    <Typography variant="h6" color="inherit" component="div">
+      LitSearch
+    </Typography>
+  </Toolbar>
+</AppBar>
   );
 }
