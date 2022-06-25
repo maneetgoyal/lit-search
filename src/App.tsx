f146d707934fa68d8dc21684036f5674b4e38693
Maneet Goyal <maneetgoyal14@gmail.com>
2022-06-25 23:47:35 +0530

    Add getDummyData

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { getDummyData } from "./utils";
 
 export function App(): JSX.Element {
   return (
-    <AppBar position="static">
-      <Toolbar variant="dense">
-        <Typography variant="h6" color="inherit" component="div">
-          LitSearch
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" component="div">
+            LitSearch
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {getDummyData().map((ele) => {
+        return JSON.stringify(ele, undefined, 2);
+      })}
+    </>
   );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,23 @@
-export interface Publication {
+import { faker } from "@faker-js/faker";
+
+// Make random data deterministic
+faker.mersenne.seed(0);
+
+interface Publication {
   publicationDate: string;
   authors: string[];
   topic: string;
+}
+
+export function getDummyData(): Publication[] {
+  const count = 1000;
+  const publications: Publication[] = [];
+  for (let i = 0; i < count; i += 1) {
+    publications.push({
+      publicationDate: faker.date.between("2020-01-01T00:00:00.000Z", "2030-01-01T00:00:00.000Z").toISOString(),
+      authors: faker.helpers.uniqueArray(faker.name.firstName, faker.datatype.number({ min: 1, max: 4 })),
+      topic: faker.color.human(),
+    });
+  }
+  return publications;
 }
