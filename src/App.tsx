97d162214bf6586729b3cb83078896a061d50b54
Maneet Goyal <maneetgoyal14@gmail.com>
2022-06-26 09:28:00 +0530

    Add match-sroter for keyword based filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { AppBar, Toolbar, Typography, Grid, TextField } from "@mui/material";
 import { rollups } from "d3-array";
 import ReactECharts from "echarts-for-react";
+import { matchSorter } from "match-sorter";
+import { useState, useEffect } from "react";
 import { getDummyData } from "./utils";
 import type { Publication } from "./utils";
+import type { TextFieldProps } from "@mui/material";
 
 function getBarChartData(
   data: Publication[],
@@ -21,7 +24,8 @@ function getBarChartData(
 }
 
 function getPieChartData(data: Publication[], filter?: string): [string, number][] {
-  const authors = data.flatMap((ele) => {
+  const filteredData = matchSorter(data, filter ?? "", { keys: ["topic"] });
+  const authors = filteredData.flatMap((ele) => {
     return ele.authors;
   });
   const aggregatedByAuthor = rollups(
@@ -40,9 +44,18 @@ function getPieChartData(data: Publication[], filter?: string): [string, number]
 }
 
 export function App(): JSX.Element {
-  const data = getDummyData();
+  const [filter, setFilter] = useState("");
+  const [data, setData] = useState<Publication[]>([]);
+  useEffect(() => {
+    const dummyData = getDummyData();
+    setData(dummyData);
+  }, []);
   const aggregatedByTime = getBarChartData(data);
-  const aggregatedByAuthor = getPieChartData(data);
+  const aggregatedByAuthor = getPieChartData(data, filter);
+  const onFilterChange: TextFieldProps["onChange"] = (evt) => {
+    const { value } = evt.target;
+    setFilter(value);
+  };
   return (
     <>
       <AppBar position="static">
@@ -52,126 +65,133 @@ export function App(): JSX.Element {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Grid container sx={{ padding: "1%" }}>
-        <Grid container item xs={12} sm={12} md={12} lg>
-          <ReactECharts
-            style={{ height: "60vh", width: "100%" }}
-            option={{
-              title: {
-                text: "Publications Timeline",
-                subtext: "By Year",
-                left: "center",
-              },
-              tooltip: {
-                trigger: "axis",
-                axisPointer: {
-                  type: "shadow",
-                },
-              },
-              grid: {
-                left: "3%",
-                right: "4%",
-                bottom: "4%",
-                containLabel: true,
-              },
-              dataset: [
-                {
-                  source: aggregatedByTime.map(([category, value]) => {
-                    return { category, value };
-                  }),
-                },
-                {
-                  transform: {
-                    type: "sort",
-                    config: { dimension: "category", order: "asc" },
-                  },
-                },
-              ],
-              xAxis: [
-                {
-                  type: "category",
-                  axisTick: {
-                    alignWithLabel: true,
-                  },
-                  name: "Time",
-                  nameLocation: "middle",
-                  nameGap: 30,
-                },
-              ],
-              yAxis: [
-                {
-                  type: "value",
-                  name: "Publications Count",
-                  nameLocation: "middle",
-                  nameGap: 30,
-                },
-              ],
-              series: [
-                {
-                  name: "Publications Count",
-                  type: "bar",
-                  showBackground: true,
-                  barWidth: "60%",
-                  encode: {
-                    x: "category",
-                    y: "value",
-                  },
-                  datasetIndex: 1,
-                },
-              ],
-            }}
-          />
-        </Grid>
-        <Grid container item xs={12} sm={12} md={12} lg direction="column" spacing={2}>
-          <Grid container item>
-            <TextField fullWidth label="Filter by keywords" type="search" />
-          </Grid>
-          <Grid container item>
+      {data.length > 0 ? (
+        <Grid container sx={{ padding: "1%" }}>
+          <Grid container item xs={12} sm={12} md={12} lg>
             <ReactECharts
               style={{ height: "60vh", width: "100%" }}
               option={{
-                color: [
-                  "#8dd3c7",
-                  "#ffffb3",
-                  "#bebada",
-                  "#fb8072",
-                  "#80b1d3",
-                  "#fdb462",
-                  "#b3de69",
-                  "#fccde5",
-                  "#d9d9d9",
-                  "#bc80bd",
-                ],
                 title: {
-                  text: "Top 10 Authors",
-                  subtext: "By Topic of Interest",
+                  text: "Publications Timeline",
+                  subtext: "By Year",
                   left: "center",
                 },
                 tooltip: {
-                  trigger: "item",
+                  trigger: "axis",
+                  axisPointer: {
+                    type: "shadow",
+                  },
                 },
+                grid: {
+                  left: "3%",
+                  right: "4%",
+                  bottom: "4%",
+                  containLabel: true,
+                },
+                dataset: [
+                  {
+                    source: aggregatedByTime.map(([category, value]) => {
+                      return { category, value };
+                    }),
+                  },
+                  {
+                    transform: {
+                      type: "sort",
+                      config: { dimension: "category", order: "asc" },
+                    },
+                  },
+                ],
+                xAxis: [
+                  {
+                    type: "category",
+                    axisTick: {
+                      alignWithLabel: true,
+                    },
+                    name: "Time",
+                    nameLocation: "middle",
+                    nameGap: 30,
+                  },
+                ],
+                yAxis: [
+                  {
+                    type: "value",
+                    name: "Publications Count",
+                    nameLocation: "middle",
+                    nameGap: 30,
+                  },
+                ],
                 series: [
                   {
-                    name: "Publications Distribution",
-                    type: "pie",
-                    radius: "50%",
-                    data: aggregatedByAuthor.map(([name, value]) => {
-                      return { name, value };
-                    }),
-                    emphasis: {
-                      itemStyle: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: "rgba(0, 0, 0, 0.5)",
-                      },
+                    name: "Publications Count",
+                    type: "bar",
+                    showBackground: true,
+                    barWidth: "60%",
+                    encode: {
+                      x: "category",
+                      y: "value",
                     },
+                    datasetIndex: 1,
                   },
                 ],
               }}
             />
           </Grid>
+          <Grid container item xs={12} sm={12} md={12} lg direction="column" spacing={2}>
+            <Grid container item>
+              <TextField
+                fullWidth
+                label="Filter by keywords"
+                type="search"
+                onChange={onFilterChange}
+              />
+            </Grid>
+            <Grid container item>
+              <ReactECharts
+                style={{ height: "60vh", width: "100%" }}
+                option={{
+                  color: [
+                    "#8dd3c7",
+                    "#ffffb3",
+                    "#bebada",
+                    "#fb8072",
+                    "#80b1d3",
+                    "#fdb462",
+                    "#b3de69",
+                    "#fccde5",
+                    "#d9d9d9",
+                    "#bc80bd",
+                  ],
+                  title: {
+                    text: "Top 10 Authors",
+                    subtext: "By Topic of Interest",
+                    left: "center",
+                  },
+                  tooltip: {
+                    trigger: "item",
+                  },
+                  series: [
+                    {
+                      name: "Publications Distribution",
+                      type: "pie",
+                      radius: "50%",
+                      data: aggregatedByAuthor.map(([name, value]) => {
+                        return { name, value };
+                      }),
+                      emphasis: {
+                        itemStyle: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: "rgba(0, 0, 0, 0.5)",
+                        },
+                      },
+                    },
+                  ],
+                }}
+              />
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
+      ) : null}
     </>
   );
 }
